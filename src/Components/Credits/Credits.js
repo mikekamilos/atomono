b96b3ad1c53a08ecccaee6e49f30b3eee9b7999c
Michael <michael.kamilos@wgu.edu>
2021-02-01 08:58:24 -0700

    expanding style features on home page

---
+++ src/Components/Credits/Credits.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Credits.css';
+
+export class Credits extends Component {
+    render() {
+        return (
+            <div className="credits">
+                <a href="https://icons8.com/icon/44024/menu">Menu icon by Icons8</a>
+            </div>
+        )
+    }
+}
+
+export default Credits
